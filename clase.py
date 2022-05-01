cfe20ccf82c69662eb747e843c574c914dbba96c
FrancoFlores1101 <103612128+FrancoFlores1101@users.noreply.github.com>
2022-04-30 21:07:53 -0300

    Add files via upload

---
+++ clase.py
@@ -0,0 +1,25 @@
+class viajero:
+    __numviajero= ''
+    __dni= ''
+    __nombre=''
+    __apellido = ''
+    __millasacum = 0
+    def __init__(self,numviajero,dni,nombre,apellido,millas):
+        self.__numviajero=numviajero
+        self.__dni=dni
+        self.__nombre=nombre
+        self.__apellido=apellido
+        self.__millasacum=int(millas)
+    def getid(self):
+        return(self.__numviajero)
+    def cantidadtotal(self):
+        return self.__millasacum
+    def acumularmillas(self,n):
+        self.__millasacum=slef.__millasacum+n
+        return (self.__millasacum)
+    def canjearmillas(self,m):
+        if(m > self.__millasacum):
+            return(print('La cantidad de millas a canjear no supera la cantidad de millas acumuladas'))
+        else:
+            self.__millasacum= self.__millasacum - m
+            return(self.__millasacum)
